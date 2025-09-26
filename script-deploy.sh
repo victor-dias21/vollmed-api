5e4f13a1a783a661eea452dc4b815d1a2b5b4349
Victor Dias <66388678+victor-dias21@users.noreply.github.com>
2025-09-26 19:01:32 -0300

    Create script-deploy.sh

---
+++ script-deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+docker load -i vollmed-api.tar
+mv docker-compose-pro.yaml docker-compose.yaml
+
+container_ids=$(docker ps -q)
+
+if [ -z "$container_ids" ]; then
+  echo "Não existem containers em execução"
+else
+  for container_id in $container_ids; do
+    echo "Parando container: " $container_id"
+    docker stop $container_id
+  done
+  echo "Todos os containers em execução foram parados"
+fi
+
+docker compose up -d
